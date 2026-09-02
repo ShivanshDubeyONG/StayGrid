84897f7d0a271bab2b6194679dd42aac2f4a8025
ShivanshDubeyONG <shivansh1.mitblr2024@learner.manipal.edu>
2026-09-02 15:32:30 +0530

    Fix login redirect

--- config/settings.py
+++ config/settings.py
@@ -186,3 +186,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # =========================================================
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "/users/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
